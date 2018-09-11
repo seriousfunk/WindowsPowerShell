47f43e07c2e375a1ed747a8cfcd5ca6939ec1468
Matt Muro <seriousfunk@users.noreply.github.com>
2018-09-11 13:07:39 -0400

    spacing

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,10 @@
 # Get Path to Microsoft.PowerShell_profile.ps1
+
 $wpsPath = Split-Path -Path $profile
 
+
 # ALIAS Definitions 
+
 set-alias -name gh -value get-help 
 set-alias -name c -value cls
 set-alias -name cl -value $wpsPath\my_scripts\cls-dir.ps1
@@ -9,11 +12,12 @@ Set-alias -name path -value $wpsPath\my_scripts\show-env-path.ps1
 set-alias -name cdi -value $wpsPath\my_scripts\docker-images.ps1
 set-alias -name cdc -value $wpsPath\my_scripts\docker-containers.ps1
 
-################################################################################# 
+
 # BANNER 
-#################################################################################
+
+# You can change or add fortune files at $wpsPath\fortunes
 function fortune {
-	$fortuneFile = "$wpsPath\fortunes\fortunes.txt"
+	$fortuneFile = "$wpsPath\fortunes\politics.txt"
 	if (Test-Path $fortuneFile -PathType Leaf) {
 		$fortunes = Get-Content $fortuneFile -raw
 		Get-Random $fortunes.split('%')
@@ -27,3 +31,4 @@ function fortune {
 
 # Remove the line below if you do not want fortune to run when PowerShell starts
 Write-host ""; fortune | Write-host -foregroundcolor red; Write-host ""
+
