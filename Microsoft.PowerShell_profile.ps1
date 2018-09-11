10dd7e29549677bc64b28b3d6bfc84f4a931dd65
Matt Muro <seriousfunk@users.noreply.github.com>
2018-09-11 12:00:57 -0400

    using variable for PowerShell profile path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,41 +1,25 @@
+# Get Path to Microsoft.PowerShell_profile.ps1
+$wpsPath = Split-Path -Path $profile
+
 # ALIAS Definitions 
 set-alias -name gh -value get-help 
 set-alias -name c -value cls
-set-alias -name cl -value $home\OneDrive\Documents\WindowsPowerShell\my_scripts\cls-dir.ps1
-Set-alias -name path -value $home\OneDrive\Documents\WindowsPowerShell\my_scripts\show-env-path.ps1
-set-alias -name cdi -value $home\OneDrive\Documents\WindowsPowerShell\my_scripts\docker-images.ps1
-set-alias -name cdc -value $home\OneDrive\Documents\WindowsPowerShell\my_scripts\docker-containers.ps1
-
-# FUNCTIONS 
-#get detailed help 
-Function GHD 
-{ 
-	CLS             
-	Get-help $args[0] -detailed | more 
-} #END GHD Function
-
-# VARIABLES 
-# $DEV = "C:\dev"
-
-# change home directory
-cd $home
+set-alias -name cl -value $wpsPath\my_scripts\cls-dir.ps1
+Set-alias -name path -value $wpsPath\my_scripts\show-env-path.ps1
+set-alias -name cdi -value $wpsPath\my_scripts\docker-images.ps1
+set-alias -name cdc -value $wpsPath\my_scripts\docker-containers.ps1
 
 ################################################################################# 
 # BANNER 
 #################################################################################
+function fortune {
+	$fortunes = Get-Content $wpsPath\my_scripts\fortune.txt -raw
+	Get-Random $fortunes.split('%')
+}
+
+# Remove the line below if you do not want fortune to run when PowerShell starts
+Write-host ""; fortune | Write-host -foregroundcolor red; Write-host ""
 
 Write-host ""
 Write-host -foregroundcolor red "Without your space helmet, Dave? You're going to find that rather difficult." 
-#Write-host -foregroundcolor green "USEFUL COMMANDS" 
-#Write-host -foregroundcolor yellow "  GET-HELP (Get-HELP ABOUT_*)" 
-#Write-host -foregroundcolor yellow "  GET-HELP GET-SERVICE -Detailed" 
-#Write-host -foregroundcolor yellow "  GET-COMMAND" 
-#Write-host -foregroundcolor yellow "  GET-MODULE -ListAvailable" 
-Write-host ""
 
-# test
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
