8568dac7a354d6ab0e39789e91c8aeb31792a873
Matt Muro <seriousfunk@users.noreply.github.com>
2018-09-21 14:17:46 -0400

    Remove-Item with -Force to del hidden .git dir

--- powershell_flair.ps1
+++ powershell_flair.ps1
@@ -1,13 +1,14 @@
 # Backup existing Windows $PROFILE directory if it exists
 if (Test-Path -Path $PROFILE) {
     Copy-Item "$(Split-Path -Path $profile)" -Destination "$(Split-Path -Path $profile)_backup_$(get-date -f yyyymmdd_HHmmss)" -Recurse
+    Remove-Item "$(Split-Path -Path $profile)" -Recurse -Force
 }
 
 # Create a new Windows $PROFILE directory
 New-Item -Type File -Path $PROFILE -Force
 
 # Delete the empty Microsoft.PowerShell_profile.ps1 the previous command created
-Remove-Item "$(Split-Path -Path $profile)\*" -Recurse
+Remove-Item "$(Split-Path -Path $profile)" -Recurse -Force
 
 # Clone these scripts into your Windows $PROFILE directory
 git clone https://github.com/seriousfunk/WindowsPowerShell.git $(Split-Path -Path $profile)
